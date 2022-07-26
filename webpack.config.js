45d6e1a07b5142f38da775e56525fca2492502d6
deplorable <codebogan@gmail.com>
2022-07-26 19:43:04 +1000

    Issues with: npm run start

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
       cacheGroups: {
         common: {
           test: /[\\/]node_modules[\\/]/,
-          name: "common",
+          //name: "common",
           chunks: "all",
         },
       },
